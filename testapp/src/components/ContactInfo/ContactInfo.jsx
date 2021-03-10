96e63251b0c81ffbc3b0d9b366af855bc9502c64
KoenLeth <lefonix@gmail.com>
2021-03-10 15:30:15 +0300

    Added verification for email

--- testapp/src/components/ContactInfo/ContactInfo.jsx
+++ testapp/src/components/ContactInfo/ContactInfo.jsx
@@ -5,76 +5,109 @@ import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import { Grid, TextField } from '@material-ui/core';
+import { Formik } from 'formik';
+import * as Yup from "yup";
 
 const useStyles = makeStyles((theme) => ({
-    input: {
-        '& > *': {
-            margin: theme.spacing(1),
-            width: '60%',
-        },
+  input: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '60%',
     },
+  },
 }));
 
 const ContactInfo = () => {
-    
 
-    const classes = useStyles();
-    const [age, setAge] = React.useState('');
 
-    const handleChange = (event) => {
-        setAge(event.target.value);
-    };
+  const classes = useStyles();
+  const [age, setAge] = React.useState('');
 
-    return <Grid container spacing = {2}>
-        <Grid item xs={6} className = {classes.input}>
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
+  const validationSchema = Yup.object({
+    email: Yup.string().email('Email is invalid')
+  });
+
+  return <Formik
+    initialValues={{ email: '', password: '' }}
+    onSubmit={() => { }}
+    validationSchema={validationSchema}
+  >
+    {({
+      values,
+      errors,
+      touched,
+      handleChange,
+      handleBlur,
+      handleSubmit,
+      isSubmitting,
+      /* and other goodies */
+    }) => (
+      <form onSubmit={handleSubmit}>
+        <Grid container spacing={2}>
+          <Grid item xs={6} className={classes.input}>
             <FormControl variant="outlined">
-                <InputLabel id="demo-simple-select-outlined-label">Age</InputLabel>
-                <Select
-                    labelId="demo-simple-select-outlined-label"
-                    id="demo-simple-select-outlined"
-                    value={age}
-                    onChange={handleChange}
-                    label="Age"
-                >
-                    <MenuItem value="">
-                        <em>None</em>
-                    </MenuItem>
-                    <MenuItem value={10}>Ten</MenuItem>
-                    <MenuItem value={20}>Twenty</MenuItem>
-                    <MenuItem value={30}>Thirty</MenuItem>
-                </Select>
+              <InputLabel id="demo-simple-select-outlined-label">Age</InputLabel>
+              <Select
+                labelId="demo-simple-select-outlined-label"
+                id="demo-simple-select-outlined"
+                value={age}
+                onChange={handleChange}
+                label="Age"
+              >
+                <MenuItem value="">
+                  <em>None</em>
+                </MenuItem>
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+              </Select>
             </FormControl></Grid>
-        <Grid item xs={6} className = {classes.input}>
+          <Grid item xs={6} className={classes.input}>
+            <TextField
+              id="email"
+              label="Email"
+              variant="outlined"
+              value={values.email}
+              onChange={handleChange}
+              error={touched && errors.email}
+              helperText={errors.email} 
+              placeholder="example@mail.ru"/>
+          </Grid>
+          <Grid item xs={6} className={classes.input}>
             <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+          </Grid>
+          <Grid item xs={6} className={classes.input}>
+            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+          </Grid>
+          <Grid item xs={6} className={classes.input}>
+            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+          </Grid>
+          <Grid item xs={6} className={classes.input}>
+            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+          </Grid>
+          <Grid item xs={6} className={classes.input}>
+            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+          </Grid>
+          <Grid item xs={6} className={classes.input}>
+            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+          </Grid>
+          <Grid item xs={6} className={classes.input}>
+            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+          </Grid>
+          <Grid item xs={6} className={classes.input}>
+            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+          </Grid>
+          <Grid item xs={6} className={classes.input}>
+            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+          </Grid>
         </Grid>
-        <Grid item xs={6} className = {classes.input}>
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-        </Grid>
-        <Grid item xs={6} className = {classes.input}>
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-        </Grid>
-        <Grid item xs={6} className = {classes.input}>
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-        </Grid>
-        <Grid item xs={6} className = {classes.input}>
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-        </Grid>
-        <Grid item xs={6} className = {classes.input}> 
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-        </Grid>
-        <Grid item xs={6} className = {classes.input}>
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-        </Grid>
-        <Grid item xs={6} className = {classes.input}>
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-        </Grid>
-        <Grid item xs={6} className = {classes.input}>
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-        </Grid>
-        <Grid item xs={6} className = {classes.input}>
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-        </Grid>
-    </Grid>
+      </form>
+    )}
+  </Formik>
 }
 
 export default ContactInfo;
